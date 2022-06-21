d0275fa494e8c557fc761256e3be62b25419b1b4
Woosuk Oh <ows@elicer.com>
2022-06-21 22:36:40 +0900

    v0.1 playwright

---
+++ playwright.config.ts
@@ -0,0 +1,17 @@
+import {devices, PlaywrightTestConfig} from "@playwright/test";
+
+const config: PlaywrightTestConfig = {
+    forbidOnly: !!process.env.CI,
+    retries: process.env.CI ? 2 : 0,
+    use: {
+        trace: 'on-first-retry',
+    },
+    projects: [
+        {
+            name: 'chromium',
+            use: { ...devices['Desktop Chrome ']}
+        }
+    ]
+};
+
+export default config;
